87bd607893d4e9efe29fa2d69c6a6747fe1ba694
Liliana Tapia <lili.tapia@duocuc.cl>
2025-12-15 01:21:45 -0300

    chore: ajustar base para gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FRONTEND2-SUM2/',
+  base: '/FRONTEND2-SUM3/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
